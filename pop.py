8f75c7b999e384fdfcb8f0a6685f3ed5cdca855a
prajwalbiradar757 <42932937+prajwalbiradar757@users.noreply.github.com>
2018-09-17 16:18:58 +0530

    Add files via upload

---
+++ pop.py
@@ -0,0 +1,13 @@
+a=int(input("enter the number"))
+if a>=90:
+	print("A+")
+elif a>=80:
+	print("A")
+elif a>=70:
+	print("B+")
+elif a>=60:
+	print("B")
+elif a>=50:
+	print("C+")
+else:
+	print("fail")
