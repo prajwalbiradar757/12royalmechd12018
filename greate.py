ffd85522d407d99618e50fe6c5de80c9f27266a0
prajwalbiradar757 <42932937+prajwalbiradar757@users.noreply.github.com>
2018-09-17 16:18:40 +0530

    Add files via upload

---
+++ greate.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number A"))
+b=int(input("enter the number B"))
+c=int(input("enter the number C"))
+if (a>b & a>c ):
+	print ("A is greater than B & C")
+elif (b>c):
+	print("B is greater")
+else:
+	print("C is greater")
